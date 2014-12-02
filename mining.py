1f52048961b078354a44db524593020f77b2c399
schulthe <sarah.schultheis@mail.utoronto.ca>
2014-12-01 21:01:29 -0500

    completed function

--- mining.py
+++ mining.py
@@ -12,7 +12,7 @@ __status__ = "Prototype"
 
 # imports one per line
 import json
-import datetime
+import operator
 
 stock_data = []
 monthly_averages = []
@@ -20,9 +20,10 @@ monthly_averages = []
 
 def read_stock_data(stock_name, stock_file_name):
     daily_stock = read_json_from_file(stock_file_name)
-    #sort daily_stock by date? or do we assume its chronological order
+    monthly_averages.sort(key=operator.itemgetter(0),reverse=False)
     average_price_numerator = 0
     average_price_denominator = 0
+    del monthly_averages[:]
     comparison_month = ""
     for ele in daily_stock:
         current_month = ele.get("Date")[0:7]
@@ -34,7 +35,7 @@ def read_stock_data(stock_name, stock_file_name):
         else:
             monthly_average_price = average_price_numerator / average_price_denominator
             formatted_date = comparison_month.replace("-","/")
-            monthly_averages.append((formatted_date, monthly_average_price))
+            monthly_averages.append((formatted_date, round(monthly_average_price,2)))
             comparison_month = current_month
             average_price_numerator = (ele.get("Volume") * ele.get("Close"))
             average_price_denominator = (ele.get("Volume"))
@@ -42,40 +43,29 @@ def read_stock_data(stock_name, stock_file_name):
     #final month calculation
     monthly_average_price = average_price_numerator / average_price_denominator
     formatted_date = comparison_month.replace("-","/")
-    monthly_averages.append((formatted_date, monthly_average_price))
+    monthly_averages.append((formatted_date, round(monthly_average_price,2)))
 
-    return
-
-def getKey(item):
-    return item [1]
-
-    for sales_average in stock.monthly_averages.values():
-list_of_averages.append(sales_average)
-list_of_averages.sort(reverse=sort_order)
-for this_average in list_of_averages[:6]:
-for year_month, sales_average in stock.monthly_averages.items():
-if this_average == sales_average:
-results.append((year_month, sales_average))
-
+    return monthly_averages
 
 
 def six_best_months():
-    if monthly_averages = # empty - look into whether list is empty
+    if not monthly_averages:
         return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
-    #if...
+    monthly_averages.sort(key=operator.itemgetter(1),reverse=True)
+    return monthly_averages[:6]
 
-# all data will be referenced in monthly_averages[]
-#sort by stock value and take beginning (or last) 6 elements
-# or i could compare elemenets in a for loop and decides to push an element in top 6 or out
-#
 
 def six_worst_months():
-    if monthly_averages = # empty look into determing whether list is empty
+    if not monthly_averages:
         return [('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0), ('', 0.0)]
-    #if...
+    monthly_averages.sort(key=operator.itemgetter(1),reverse=False)
+    if len(monthly_averages) < 6:
+        return ("Data Error")
+    return monthly_averages[:6]
+
 
 def read_json_from_file(file_name):
-    with open(file_name) as file_handle:
+    with open(file_name, "r") as file_handle:
         file_contents = file_handle.read()
 
     return json.loads(file_contents)
